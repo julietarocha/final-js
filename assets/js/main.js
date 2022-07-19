18005892fcdd83cd5f17ccda89c114f99f4346d5
julieta-rocha <juulietaroocha@gmail.com>
2022-07-19 18:05:51 -0300

    commit all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,20 @@
-const nombre = prompt ("Ingrese su nombre")
-let precio = 0
-alert ("Bienvenido, " + nombre  + "!. ¿Que estas buscando?")
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
 
-let calzado = prompt ("Ingrese tipo de calzado (1 = bota, 2 = borcego)")
-while (calzado != 1 && calzado != 2) {
-    alert ("El calzado no existe")
-    calzado = prompt ("Ingrese tipo de calzado (1 = bota, 2 = borcego)")
-}
-if (calzado == 1){
-    precio = 8650
-}else {
-    precio = 9863
-}
-alert ("El precio del calzado es $" + precio)
-let dinero = prompt ("¿Con cuanto abonas?")
-while (dinero < precio){
-    alert ("El pago es insuficiente")
-    dinero = prompt ("¿Con cuanto abonas?")
-}
+console.log(menu);
+console.log(hamburger);
 
-let vuelto = dinero - precio
-alert ("Su pago es de $" + precio + " y su cambio es $" + vuelto + " ¡GRACIAS POR SU COMPRA!")
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle("spread")
+});
 
-console.log(vuelto(precio - dinero))
-function vuelto(parametro1, parametro2){
-    return parametro1 - parametro2
-}
+window.addEventListener('click', e=>{
+    if(menu.classList.contains('spread')
+        && e.target != menu && e.target != hamburger){
+            menu.classList.toggle("spread")
+    }
+});
+
+
 
 
