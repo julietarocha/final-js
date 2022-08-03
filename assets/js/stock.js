eafd137e6cd1261379d4eff50075d6b82bcb5800
julieta-rocha <juulietaroocha@gmail.com>
2022-08-02 21:22:51 -0300

    commit ecommerce uno

---
+++ assets/js/stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, nombre: "Labial 1", tipo: "labial", desc: "Tono nude", precio: 1975, img: 'imagenes/labial1.jpg'},
+    {ig: 2, nombre: "Labial 2", tipo: "labial", desc: "Tono rojo", precio: 1975, img: 'imagenes/labial2.jpg'},
+    {id: 3, nombre: "Labial 3", tipo: "labial", desc: "Tono nude", precio: 1975, img: 'imagenes/labial1.jpg'},
+    {ig: 4, nombre: "Pincel 1", tipo: "pincel", desc: "Para rubor", precio: 2850, img: 'imagenes/pincel1.jpg'},
+    {id: 5, nombre: "Pincel 2", tipo: "pincel", desc: "Para sombras", precio: 2850, img: 'imagenes/pincel2.jpg'},
+    {ig: 6, nombre: "Pincel 3", tipo: "pincel", desc: "Para base", precio: 2850, img: 'imagenes/pincel3.jpg'},
+    {id: 7, nombre: "Serum 1", tipo: "serum", desc: "Para piel seca", precio: 5600, img: 'imagenes/serum1.jpg'},
+    {ig: 8, nombre: "Serum 2", tipo: "serum", desc: "Para piel grasosa", precio: 5600, img: 'imagenes/serum2.jpg'},
+    {id: 9, nombre: "Serum 3", tipo: "serum", desc: "Para piel mixta", precio: 5600, img: 'imagenes/serum3.jpg'},
+]
