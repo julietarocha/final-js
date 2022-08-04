29ce440642f1df268807484e66d97fbb7ea94b41
julieta-rocha <juulietaroocha@gmail.com>
2022-08-04 11:59:11 -0300

    commit stock

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 let carritoCompras = []
 
 let contenedorProductos = document.getElementById('contenedor-productos');
@@ -7,20 +8,19 @@ function mostrarProductos() {
     stockProductos.forEach(item => {
         let div = document.createElement('div')
         div.className = 'producto'
-        div.innerHTML = ` 
-                            <div class="card">
-                                <img src="${item.img}" class="card-img-top" alt="lab1-nude">
-                                <div class="card-body">
-                                    <h5 class="card-title">${item.nombre}</h5>
-                                    <p class="card-text">${item.desc}</p>
-                                    <p class="precio">$${item.precio}</p>
-                                    <a href="#" class="btn btn-primary">Agregar al carrito</a>
-                                </div>
-                            </div>  
-                        `
+        div.innerHTML = ` <div class="card">
+                            <img src="${item.img}" class="card-img-top" alt="lab1-nude">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.nombre}</h5>
+                                <p class="card-text">${item.desc}</p>
+                                <p class="precio">$${item.precio}</p>
+                                <a href="#" class="btn btn-primary">Agregar al carrito</a>
+                            </div>
+                        </div>`
         contenedorProductos.appendChild(div)
     })
 }
+
 
 
 
